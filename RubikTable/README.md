6dd4df860ad7fdf7c4a9c5a7d40c4ed82ec87a12
hemantharaj95 <hemantharaj95@gmail.com>
2023-02-06 22:30:48 +0530

    Initially start with Rubix Table MAPF algorithm.

---
+++ RubikTable/README.md
@@ -0,0 +1,3 @@
+## Rubik Tables and Object Rearrangement
+
+Implementation of MAPF algorithm based on the paper [Rubik Tables and Object Rearrangement](https://arxiv.org/pdf/2002.04979.pdf).
---
+++ RubikTable/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+void columnFitting()
+{
+
+}
+
+void rowFitting()
+{
+
+}
+int main()
+{
+    std::cout << "Hello World\n";
+    
+    //Red[1,2,3,4], Green[5,6,7,8], Blue[9,10,11,12] and Orange[13,14,15,16]
+    std::vector<std::vector<int>>  stacks = {{6,4,10,12},{5,8,1,3},{13,15,9,14},{11,16,7,2}};
+}
